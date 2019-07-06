0ae119111bcbaf63de5ee68390d73249a856a2eb
cruz ferrer <cruz.ferrer2@gmail.com>
2019-07-06 12:50:22 -0400

    jwt

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
+
+const jwt = require('jsonwebtoken');
+
 var db = mongojs('mongodb://userdb:userdb@ds163718.mlab.com:63718/cferrerdb', ['tasks']);
 //mongodb://cferrer:cferrer123@ds163718.mlab.com:63718/cferrerdb
+
+//jwt calls..
+
+router.post('/login',function(req,res) {
+    let user = req.body;
+    jwt.sign({user},'dotenv',(err,token)=>{
+        res.json({
+            token,
+            user
+        })
+    })
+})
 // Get All Tasks
+
 router.get('/',function(req, res, next){
     db.tasks.find(function(err, tasks){
         if(err){
@@ -16,7 +32,7 @@ router.get('/',function(req, res, next){
 });
 
 // Get Single Task
-router.get('/:id', function(req, res, next){
+router.get('/:id', verifyToken, (req, res, next) => {
     db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
         if(err){
             res.send(err);
@@ -25,23 +41,39 @@ router.get('/:id', function(req, res, next){
     });
 });
 
-//Save Task
-router.post('/', function(req, res, next){
-    var task = req.body;
-    console.log('save task ' + JSON.stringify(req.body));
-    if(!task.Title){
-        res.status(400);
-        res.json({
-            "error": "Bad Data"
-        });
-    } else {
-        db.tasks.save(task, function(err, task){
-            if(err){
-                res.send(err);
-            }
-            res.json(task);
-        });
+function verifyToken(req,res,next){
+    const bearHeader = req.headers['authorization'];
+    if(typeof bearHeader !=='undefined'){
+        const bearer = bearHeader.split(' ');
+        //get token
+        const bearerToken = bearer[1];
+        //set the token
+        req.token = bearerToken;
+        next();
+
+    }else{
+        res.sendStatus(403);
     }
+
+}
+//Save Task
+router.post('/', verifyToken, function(req, res, next){
+    var task = req.body;
+    console.log('saving task ' + JSON.stringify(req.body));
+    jwt.verify(req.token,process.env.JWT_SECRET,(err,authData) =>{
+        if(err){
+            res.sendStatus(403)
+        }else{
+            db.tasks.save(task, (err, task) =>{
+                        if(err){
+                            res.send(err);
+                        }
+                        res.json(task);
+            });
+        }
+    })
+       
+    
 });
 
 // Delete Task
